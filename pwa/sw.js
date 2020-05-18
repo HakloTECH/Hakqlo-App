bf8052c222a2742e0436a9c3487949c1112abb86
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-18 23:05:27 +0900

    organized file structure

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.01",OLD_CACHE_NAME = ["mi-kenqjo-app"];
+const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.011";
 const CACHE_NAME = APP_NAME+'-'+APP_VERSION;
 const FILES_TO_CACHE = [
   '../',
