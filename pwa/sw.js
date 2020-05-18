8f4a43ce801ffe745e2d5b369b0d1173c90c5f70
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-18 22:42:56 +0900

    organized file structure

--- pwa/sw.js
+++ pwa/sw.js
@@ -19,7 +19,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
     .then((cache)=> {
         console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
+        return cache.addAll(FILES_TO_CACHE.map(url => new Request(url, {credentials: 'same-origin'})));
     })
   );
 });
