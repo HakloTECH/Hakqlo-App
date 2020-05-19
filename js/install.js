424522e1161934e32cbd8a74e04794f31626405a
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-19 18:14:52 +0900

    fixed sw

--- js/install.js
+++ js/install.js
@@ -1,7 +1,7 @@
 ((global)=>{
     if ('serviceWorker' in navigator) {
 
-        navigator.serviceWorker.register('./pwa/sw.js').then(function() {
+        navigator.serviceWorker.register('./sw.js').then(function() {
             console.log('sucsessed');
         }).catch(function(err) {
             console.log('error:', err);
