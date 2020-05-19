e64a6a000663ca03491826d29a4a96acce6e2aad
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-19 22:55:30 +0900

    sw channel messaging connection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.029";
+const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.030";
 const CACHE_NAME = APP_NAME+'-'+APP_VERSION;
 const FILES_TO_CACHE = [
   './',
