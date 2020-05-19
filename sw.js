6ce5a34a62ebef89288f2d9ce0a4d112c4284991
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-19 18:21:00 +0900

    fixed file structures

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.013";
+const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.014";
 const CACHE_NAME = APP_NAME+'-'+APP_VERSION;
 const FILES_TO_CACHE = [
   './',
