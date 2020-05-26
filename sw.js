148e438ebef8ce229aba7fc5ef81c975d98a29c8
Momijiichigo <ichigomomiji436@gmail.com>
2020-05-26 21:45:40 +0900

    dialog style

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.03445";
+const APP_NAME = "hakqlo-app", APP_VERSION = "0.0.03444";
 const CACHE_NAME = APP_NAME+'-'+APP_VERSION;
 const FILES_TO_CACHE = [
   './',
