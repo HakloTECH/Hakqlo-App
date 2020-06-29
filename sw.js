3ec92b3dbe1621ac9c2e8a3b3398f4c0a62b5c72
Momijiichigo <ichigomomiji436@gmail.com>
2020-06-29 21:55:46 +0900

    Updates

--- sw.js
+++ sw.js
@@ -7,13 +7,13 @@ var serviceWorkerOption = {
     "./2bfa27340655858f8ebb60b791e8debb.svg",
     "./78492f0a1915464b90c8acc9c91b67cb.css",
     "./43753b6d70284ce6a52997185c34c02a.svg",
-    "./runtime.3afcda1644e40491737b.js",
+    "./runtime.815e8894aab9904a30e2.js",
     "./vendors.css",
-    "./vendors.3afcda1644e40491737b.js",
+    "./vendors.815e8894aab9904a30e2.js",
     "./app.css",
-    "./app.3afcda1644e40491737b.js",
+    "./app.815e8894aab9904a30e2.js",
     "./install.css",
-    "./install.3afcda1644e40491737b.js",
+    "./install.815e8894aab9904a30e2.js",
     "./index.html"
   ]
 };
